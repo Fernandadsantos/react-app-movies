5c631210756a7ea9829063424eca08a64841bac0
fernanda Santos <fernandassilvasantoss@gmail.com>
2023-03-08 21:26:40 -0300

    Change link

--- src/pages/Albums.tsx
+++ src/pages/Albums.tsx
@@ -18,7 +18,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Movie Catalog
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}

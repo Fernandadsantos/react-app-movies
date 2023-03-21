152cbed801a852048dd74ce0dc74a872cee88727
Fernanda <119077188+Fernandadsantos@users.noreply.github.com>
2023-03-20 22:04:00 -0300

    finish input

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,22 +1,15 @@
 import glass from '../../assets/magnifying-glass.svg';
 import './search.scss';
 
+
 function SearchBar({ onChange }: any) {
 
     return (
-        <div className="search-container">
+        <div className='search-container'>
+            <input className='search-input' onChange={onChange} placeholder='Titulos, gêneros...' />
             <img src={glass} alt="" className='search-icon' />
-            <div className="search-box"> 
-                <input className="search-txt" type="text" placeholder="Type to search" />
-                
-            </div>
         </div>
     )
-    // return (
-    //     <div className='search-container'>
-    //         <input className='search-input' onChange={onChange} placeholder='Titulos, gêneros...' /><img src={glass} alt="" className='search-icon' />
-    //     </div>
-    // )
 }
 
 export default SearchBar;

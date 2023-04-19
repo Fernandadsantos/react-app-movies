992bf81db0d4dec20342b87225fa70ba3f6c10c0
fernanda Santos <fernandassilvasantoss@gmail.com>
2023-04-19 16:16:57 -0300

    popupin progress

--- src/components/popupAlert/index.tsx
+++ src/components/popupAlert/index.tsx
@@ -1,4 +1,5 @@
 import Alert from "react-popup-alert";
+import './popupAlert.scss';
 
 interface PopUpProps {
     text: string;
@@ -10,10 +11,15 @@ interface PopUpProps {
 function PopupAlert({ text, type, show, onClosePress }: PopUpProps) {
 
     return (
-        <div>
+        <div style={{ 
+            display:'flex',
+            justifyContent:'center',
+            alignItems: 'center',
+
+            }}>
             <Alert
-                header={'Header'}
-                btnText={'Close'}
+                header={'Não foi possível realizar o cadastro'}
+                btnText={'fechar'}
                 text={text}
                 type={type}
                 show={show}
@@ -21,15 +27,26 @@ function PopupAlert({ text, type, show, onClosePress }: PopUpProps) {
                 pressCloseOnOutsideClick={true}
                 showBorderBottom={false}
                 alertStyles={{
-                    backgroundColor: 'rgba(98, 102, 101, 0.71)',
+                    background: '#141414',
                     borderRadius: '5px',
-                    padding: '50px',
+                    padding: '40px',  
+                    width:'500px',
                     textAlign: 'center',
-                    position: 'static',  
                 }}
-                headerStyles={{}}
-                textStyles={{}}
-                buttonStyles={{}}
+                headerStyles={{
+                    fontSize: '25px',
+                }}
+                textStyles={{
+                    fontSize: '20px',
+                }}
+                buttonStyles={{
+                    fontSize: '20px',
+                    backgroundColor: 'transparent',
+                    borderRadius: '3px',
+                    border: 'solid 1px', 
+                    padding: '8px', 
+                    textDecoration: 'none',
+                }}
             />
         </div>
     )
